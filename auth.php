f691e5c0fc2efbe9ecbd3953da02799df0834ac2
wzh <tianxiafengyu@qq.com>
2012-11-15 00:34:42 +0800

    add all

---
+++ auth.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<script src="http://tjs.sjs.sinajs.cn/t35/apps/opent/js/frames/client.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
+	<script type="text/javascript">
+		App.AuthDialog.show({
+			"client_id" : "1328295755",
+			"redirect_uri" : "http://apps.weibo.com/avatarguess"
+		});
+	</script>
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+define( "WB_AKEY" , '1328295755' );
+define( "WB_SKEY" , 'ac86ed54c0efcaed26e5aca507d0a629' );
+define( "CANVAS_PAGE" , "http://www.123456789.com/weibo-php/index.php" );
